fed33fc9d4012b7bbcd9dba1658b85c88bcda83f
Jesus <jesusjuanaguilar@gmail.com>
2016-02-08 23:30:08 +0100

    HBSP

---
+++ StartHBSP/src/main/java/com/program/webapp/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.program.webapp.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import com.program.webapp.service.EmployeeManager;
+ 
+public class EmployeeController implements Controller {
+	private EmployeeManager employeeManager;
+ 
+	public EmployeeManager getEmployeeManager() {
+		return employeeManager;
+	}
+ 
+	public void setEmployeeManager(EmployeeManager employeeManager) {
+		this.employeeManager = employeeManager;
+	}
+ 
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		System.out.println("in EmployeeController");
+		ModelAndView mv = new ModelAndView("employeeList");
+		mv.addObject("employeeList", this.employeeManager.getEmployeeList());
+		return mv;
+	}
+}
---
+++ StartHBSP/src/main/java/com/program/webapp/service/EmployeeManager.java
@@ -0,0 +1,21 @@
+package com.program.webapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.program.webapp.model.Employee;
+ 
+public class EmployeeManager {
+	private static List<Employee> employeeList;
+ 
+	public EmployeeManager(){
+		 employeeList = new ArrayList<Employee>();
+		 employeeList.add(new Employee("1", "Mike", "Smith"));
+		 employeeList.add(new Employee("2", "John", "Taylor"));
+		 employeeList.add(new Employee("3", "Dave", "Wilson"));	 
+	}
+ 
+	public List<Employee> getEmployeeList(){
+		return employeeList;
+	}
+}
